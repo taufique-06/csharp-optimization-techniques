e93194277a7fe884ebcbb24a5c93fc1924c77a46
Taufique Azad <taufique.azad@mountainwarehouse.com>
2025-08-25 11:27:45 +0100

    High_GC_Alloc_sample_project with solution

---
+++ High GC Allocation/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using High_GC_Allocation;
+
+Console.WriteLine("Starting High GC Allocation Test........");
+
+//TestMethods.HighGCAllocationMethod();
+
+//TestMethods.Resolve_High_GC_Allocation_Method_Using_List();
+
+//TestMethods.Resolve_High_GC_Allocation_Method_Using_ArrayPool();
+
+
+Console.WriteLine("Done.");
+
--- High GC Allocation/TestMethods.cs
+++ High GC Allocation/TestMethods.cs
@@ -1,6 +1,52 @@
+using System.Buffers;
+
 namespace High_GC_Allocation;
 
-public class TestMethods
+public static class TestMethods
 {
+    public static void HighGCAllocationMethod()
+    {
+        for (int i = 0; i < 500000; i++)
+        {
+            var data = new long[100000];
     
+            for (int j = 0; j < 100; j++)
+            {
+                data[j] = j;
+            }
+        }
+    }
+    
+    public static void Resolve_High_GC_Allocation_Method_Using_List()
+    {
+        for (int i = 0; i < 500000; i++)
+        {
+            var data = new List<long>();
+            for (int j = 0; j < 100; j++)
+            {
+                data.Add(j);
+            }
+            data.TrimExcess();
+        }
+    }
+    
+    public static void Resolve_High_GC_Allocation_Method_Using_ArrayPool()
+    {
+        var pool = ArrayPool<long>.Shared;
+        for (int i = 0; i < 500000; i++)
+        {
+            var data = pool.Rent(100);
+            try
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    data[j] = j;
+                }
+            }
+            finally
+            {
+                pool.Return(data);
+            }
+        }
+    }
 }
